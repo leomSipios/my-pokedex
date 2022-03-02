1b5aead732bbc07b837fd5714f49b887b036e464
leomSipios <leom@sipios.com>
2022-03-02 17:09:21 +0100

    tests

--- shipitfile.js
+++ shipitfile.js
@@ -9,7 +9,7 @@ module.exports = shipit => {
       repositoryUrl: 'https://github.com/leomSipios/my-pokedex.git',
     },
     staging: {
-      servers: 'leom@sipios.com',
+      servers: 'deploy@staging.super-project.com',
     },
   })
 }
